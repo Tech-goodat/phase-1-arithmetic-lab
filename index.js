d364ce19d0b57c5e4c0daffb32f3d7a93203ad8c
Tech-goodat <felixkiprotich2000@gmail.com>
2023-09-21 11:07:59 +0300

    done with maths lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=(num1*num2);
+console.log(multiply);
+
+let random=Math.floor(Math.random())*10 +1;
+
+let num3=10;
+let num4=6;
+let mod=(num3%num4);
+console.log(mod);
+let max;
+max=Math.max(1,3,4,6,7,9,20);
+console.log(max);
